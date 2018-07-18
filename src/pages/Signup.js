47b06a9a3d6cc1d709b6ed7e63f00b45b9310706
Ulisses Ferreira <ulissesvf@gmail.com>
2018-07-18 17:54:54 +0100

    style: ident some code

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -14,19 +14,19 @@ class Signup extends Component {
 
     postData = (url = ``, data = {}) => {
     // Default options are marked with *
-    return fetch(url, {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, same-origin, *omit
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        body: JSON.stringify(data), // body data type must match "Content-Type" header
-    })
+        return fetch(url, {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, cors, *same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            redirect: "follow", // manual, *follow, error
+            referrer: "no-referrer", // no-referrer, *client
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        })
         .then(response => response.json()) // parses response to JSON
         .catch(error => console.error(`Fetch Error =\n`, error));
     };
@@ -35,7 +35,7 @@ class Signup extends Component {
 
         alert('going for it')
 
-        this.postData(`http://api-env.882xcpbbwf.eu-west-2.elasticbeanstalk.com/users`, { answer: 42 })
+        this.postData(`http://api-env.882xcpbbwf.eu-west-2.elasticbeanstalk.com/users/`, { answer: 42 })
             .then(data => console.log(data)) // JSON from `response.json()` call
             .catch(error => console.error(error));
     }
