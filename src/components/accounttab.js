3e75195eac1d4b0942a1a4647ccfd0b117a73c65
Ulisses Ferreira <ulissesvf@gmail.com>
2018-07-19 14:23:21 +0100

    feat: edit account info tab

--- src/components/accounttab.js
+++ src/components/accounttab.js
@@ -2,9 +2,16 @@ import React, { Component } from 'react';
 
 class AccountTab extends Component {
     render() {
+        let email = "hello@upframe.io"
         return (
             <div>
-                Account Tab
+                <h1>Email</h1>
+                <p>Your email address is <a href={"mailto:" + email}>{email}</a>.
+                This information will not be publicly displayed</p>
+                <button>Change Email</button>
+                <h1>Password</h1>
+                <p>Set a unique password to protect your account</p>
+                <button>Change Password</button>
             </div>
         );
     }
