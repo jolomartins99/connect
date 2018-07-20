b6f28073d6de156f402249c33aef380ba079ede6
Fábio Ferreira <ffcfpten@gmail.com>
2018-07-20 17:30:49 +0100

    feat: search bar

---
+++ src/components/searchbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div class="search-bar">
+        <input type="text" />
+      </div>
+    )
+  }
+}
