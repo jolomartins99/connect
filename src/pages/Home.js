b4311d649751c454cc16e1ae0c69a25ea3de8024
Fábio Ferreira <ffcfpten@gmail.com>
2018-07-21 01:38:13 +0100

    fix: improving css organisation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -30,10 +30,11 @@ export default class Home extends Component {
         return (
             <div>
                 <Navbar search={false} />
-                <main>
+                <main id="home">
                     <div class="wrapper">
                         <div class="search">
-                            <h1>What can we help you with?</h1>
+                            <h1>Connect with Mentors</h1>
+                            <h2>and get your Startup growing</h2>
                             <input type="text" />
                             <button className="dark" onClick={this.search}>Search</button>
                             <SearchTags />
