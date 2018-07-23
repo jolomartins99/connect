a5cf03703f5f925e651c73bc59006baffe88b374
Ulisses Ferreira <ulissesvf@gmail.com>
2018-07-23 18:54:52 +0100

    feat: add free slots + delete free slots

--- src/components/synccalendartab.js
+++ src/components/synccalendartab.js
@@ -3,47 +3,54 @@ import BigCalendar from 'react-big-calendar';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import moment from 'moment';
 
+BigCalendar.setLocalizer(BigCalendar.momentLocalizer(moment));
+
 export default class SyncCalendarTab extends Component {
 
     constructor(props){
         super(props)
         this.state = {
+            currId: 0,
             events: [{
-                id: 0,
-                title: 'All Day Event very long title',
-                allDay: true,
-                start: new Date(2015, 3, 0),
-                end: new Date(2015, 3, 1),
-            },
-            {
-                id: 1,
-                title: 'Long Event',
-                start: new Date(2015, 3, 7),
-                end: new Date(2015, 3, 10),
             }]
         }
-        BigCalendar.momentLocalizer(moment); 
     }
 
-    syncGoogle = () => {
+    deleteEvent = (event) => {
+        console.log(event)
+        let listOfEvents = this.state.events
+        this.setState({
+            events: listOfEvents.filter(singleEvent => singleEvent.id !== event.id)
+        })
+    }
 
+    addFreeSlot = (slot) => {
+        let currentEvents = this.state.events
+        let currentId = this.state.currId
+        currentEvents.push({
+            id: currentId,
+            start: slot.start,
+            end: slot.end,
+            title: 'Upframe Free Slot'
+        })
+        this.setState({
+            events: currentEvents,
+            currId: currentId + 1
+        })
     }
 
     render() {
         // let allViews = Object.keys(BigCalendar.Views).map(k => BigCalendar.Views[k])
         return (
             <div className="tab-content">
-                <div className="calendar">
-                    <button onClick={this.syncGoogle}>Sync with Google</button>
-                    <BigCalendar
-                        selectable
-                        defaultView={BigCalendar.Views.WEEK}
-                        defaultDate={new Date()}
-                        events={this.state.events}
-                        startAccessor='startDate'
-                        endAccessor='endDate'
-                    />
-                </div>
+                <BigCalendar
+                    selectable
+                    defaultDate={new Date()}
+                    defaultView="week"
+                    events={this.state.events}
+                    onSelectEvent={event => this.deleteEvent(event)}
+                    onSelectSlot={slot => this.addFreeSlot(slot)}
+                />
             </div>
         );
     }
